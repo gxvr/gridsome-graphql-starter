87773f19177230fc50b4b9b3e1ca8923ffab23af
gxvr <gxvr1995@gmail.com>
2021-05-25 17:40:44 +0300

    Added graphql plugin and remote access

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,22 @@
 
 module.exports = {
   siteName: 'Gridsome-Starter',
-  plugins: []
+  plugins: [{
+    // Initialize GraphQL plugin
+    use: '@gridsome/source-graphql',
+    options: {
+
+      // GraphCMS URL 
+      url: 'your-graphqcms-api-url',
+
+      //The name that should be used to identify your remote schema.
+      // Learn more here https://gridsome.org/plugins/gridsome-source-graphql
+      fieldName: 'gcms',
+
+      //The prefix to be used for your imported schema's field types.
+      // Learn more here https://gridsome.org/plugins/gridsome-source-graphql
+      typeName: 'gcmsTypes',
+    }
+
+  }]
 }
